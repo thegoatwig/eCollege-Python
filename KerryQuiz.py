8760195c17ac85d09bd39255a1d1870d1ffff564
thegoatwig <48242326+thegoatwig@users.noreply.github.com>
2019-03-05 14:41:29 +0000

    Update KerryQuiz.py

--- KerryQuiz.py
+++ KerryQuiz.py
@@ -4,9 +4,14 @@ capitalGuess = input("What is the capital of Kerry?   ")
 numberOfGuesses = 1
 
 while capitalGuess != "Tralee":
-        numberOfGuesses = numberOfGuesses + 1
-        capitalGuess = input ("Guess Again!   ")
-if MF not in ["woman" , "lady" , "girl"]:
-    print ("You guessed it. It took you " + str(numberOfGuesses) + " guesses. Take my upvote, good sir!")
-else:
-    print("You guessed it. It took you " + str(numberOfGuesses) + " guesses. Take my upvote, good madam!")
+    numberOfGuesses = numberOfGuesses + 1
+    if numberOfGuesses > 3:
+        print ("You guessed incorrectly 3 times, fool!")
+        break
+    capitalGuess = input ("Guess Again!   ")
+    
+if numberOfGuesses <= 3:    
+    if MF not in ["woman" , "lady" , "girl"]:
+        print ("You guessed it. It took you " + str(numberOfGuesses) + " guesses. Take my upvote, good sir!")
+    else:
+        print("You guessed it. It took you " + str(numberOfGuesses) + " guesses. Take my upvote, good madam!")
