a6fce29fa26ad4f4b7444766fa81c42812bc1760
thegoatwig <48242326+thegoatwig@users.noreply.github.com>
2019-03-07 09:27:45 +0000

    Update Kladblok.py

--- Kladblok.py
+++ Kladblok.py
@@ -1,3 +1,4 @@
+#Randomizer
 import random
 number = random.randint(1, 99)
 guesses = 0
@@ -18,3 +19,36 @@ elif guess == number:
     if guess != number:
         number = str(number)
     print ("The secret number was",  number)
+    
+    
+    
+    
+#SaleGoal        
+ initialSalesGoal = 200000
+multiplier = 100
+
+for monthlyGoal in range (1,13):
+    monthlySalesGoal = initialSalesGoal + (monthlyGoal * multiplier)
+
+    print("Your sales goal for month" + str(monthlyGoal) + "is" + str(monthlySalesGoal))
+
+    
+    #KerryQuiz
+    MF = input ("Are you a man or a woman?  ").lower()
+
+capitalGuess = input("What is the capital of Kerry?   ")
+numberOfGuesses = 1
+
+while capitalGuess != "Tralee":
+    capitalGuess = input ("Guess Again!   ")
+    numberOfGuesses = numberOfGuesses + 1
+    if numberOfGuesses >= 3:
+        print ("You guessed incorrectly 3 times. Game over, fool!")
+        break
+    
+    
+if numberOfGuesses < 3:    
+    if MF not in ["woman" , "lady" , "girl"]:
+        print ("You guessed it. It took you " + str(numberOfGuesses) + " guesses. Take my upvote, good sir!")
+    else:
+        print("You guessed it. It took you " + str(numberOfGuesses) + " guesses. Take my upvote, good madam!")
